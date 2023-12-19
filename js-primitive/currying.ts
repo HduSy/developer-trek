0b133119199fd9f2874fa5262e61f6eb2bad58d3
HduSy <hdusy14032122@gmail.com>
2023-12-19 17:19:33 +0800

    feat: currying

---
+++ js-primitive/currying.ts
@@ -0,0 +1,26 @@
+/**
+ * 将一个多参数函数转化为一系列单个参数函数的技术
+ * 通俗理解：通过闭包暂存参数，参数数量足够时，才执行函数
+ */
+
+function currying(fn: Function, ...args1) {
+  if(args1.length >= fn.length) {
+    return fn(...args1)
+  } else {
+    return (...args2) => currying(fn, ...args1, ...args2)
+  }
+}
+
+// demo
+
+function add(a, b, c) {
+  return a + b + c
+}
+
+const curryingAdd = currying(add)
+
+curryingAdd(1, 2, 3) // 6
+curryingAdd(1)(2)(3) // 6
+curryingAdd(1)(2, 3) // 6
+curryingAdd(1, 2)(3) // 6
+curryingAdd()(1, 2, 3) // 6
