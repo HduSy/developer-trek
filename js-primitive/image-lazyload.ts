7e803468326556bd7c357732fc91a55500c0332a
HduSy <hdusy14032122@gmail.com>
2023-12-20 18:17:40 +0800

    feat: lazyload IntersectionObserver

--- js-primitive/image-lazyload.ts
+++ js-primitive/image-lazyload.ts
@@ -1,12 +1,11 @@
 /**
- * 
  * <img src="loading.gif" data-src="https://cdn.pixabay.com/photo/2015/09/09/16/05/forest-931706_1280.jpg" alt="">
  * <img src="loading.gif" data-src="https://cdn.pixabay.com/photo/2014/08/01/00/08/pier-407252_1280.jpg" alt="">
  * <img src="loading.gif" data-src="https://cdn.pixabay.com/photo/2014/12/15/17/16/pier-569314_1280.jpg" alt="">
  * <img src="loading.gif" data-src="https://cdn.pixabay.com/photo/2010/12/13/10/09/abstract-2384_1280.jpg" alt="">
  */
 
-
+// 1、监听高度实现
 const imgs = document.getElementsByTagName('img')
 let n = 0 // 存储图片加载到的位置，避免每次都从第一张图片开始遍历
 
@@ -24,10 +23,8 @@ function throttle(event, time) {
 }
 
 const lazyload = () => {
-  const availableViewHeight = window.innerHeight // 可见区域高度
-  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop // 已滚动高度
   for (let i = n; i < imgs.length; i++) {
-    if(imgs[i].offsetTop < availableViewHeight + scrollTop) {
+    if(imgs[i].offsetTop < window.innerHeight + document.documentElement.scrollTop || document.body.scrollTop) { // 可见区域高度 + 已滚动高度
       if(imgs[i].getAttribute('src') === 'loading.gif') { // 还未加载
         imgs[i].src = imgs[i].getAttribute('data-src')
       }
@@ -38,3 +35,29 @@ const lazyload = () => {
 
 // 页面添加滚动监听
 window.addEventListener('scroll', throttle(lazyload, 200))
+
+// 2、IO
+function lazyload() {
+  if(window.IntersectionObserver) {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const img = entry.target as HTMLImageElement
+        if(entry.isIntersecting) {
+          if(img.getAttribute('src') === 'loading.gif') {
+            img.src = img.getAttribute('data-src')
+          }
+          observer.unobserve(img) // 移除监听：停止监听已开始懒加载的img元素
+        }
+      })
+    })
+    for (let i = 0; i < imgs.length; i++) {
+        observer.observe(imgs[i]) // 逐个监听img元素
+    }
+  } else {
+    // const rect =  element.getBoundingClientRect()
+    // rect.top < window.innerHeight && rect.bottom > 0 && rect.left < window.innerWidth && rect.right > 0
+
+    // 或者
+    // element.offsetTop < window.innerHeight + document.documentElement.scrollTop || document.body.scrollTop
+  }
+}
