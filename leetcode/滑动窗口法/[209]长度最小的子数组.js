edd3b0ccfe352c67bd20ff4aa9cf3515305aaf3f
HduSy <hdusy14032122@gmail.com>
2024-03-05 21:49:16 +0800

    feat: 209

--- leetcode/滑动窗口法/[209]长度最小的子数组.js
+++ leetcode/滑动窗口法/[209]长度最小的子数组.js
@@ -21,3 +21,20 @@ var minSubArrayLen = function(target, nums) {
   }
   return res > nums.length ? 0 : res
 };
+
+var minSubArrayLen = function(target, nums) {
+  let left = 0, right = 0, res = nums.length + 1
+  let sum = 0
+  while(right < nums.length) {
+    let n = nums[right]
+    if(n >= target) return 1
+    sum += n
+    while(sum >= target) {
+      res = Math.min(res, right - left + 1)
+      sum -= nums[left]
+      left++ // 不断向右缩小窗口
+    }
+    right ++ // 不断向右扩大
+  }
+  return res > nums.length ? 0 : res
+}
